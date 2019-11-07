135f449b1a2a6ac2bd55364371bb029fa844dba3
Aceman74 <thedmx74@hotmail.fr>
2019-11-07 11:25:44 +0100

    First commit

---
+++ app/src/main/java/com/aceman/notepad/NoteListActivity.kt
@@ -0,0 +1,13 @@
+package com.aceman.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.note_list_activity)
+    }
+}
